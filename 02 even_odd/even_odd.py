a22a49f1b1b9e81db6923ed14cd5e1973f7ea2ce
MahdiDvSo <mahdiiltireh324@gmail.com>
2026-04-27 22:15:50 +0100

    02 even_odd

---
+++ 02 even_odd/even_odd.py
@@ -0,0 +1,12 @@
+# 2. Even or Odd Number
+# Idea: Check number type
+
+print('soo dhawoow jaale.')
+name = str(input('What is Your name ?'))
+print(name.capitalize())
+number = int(input('please enter your number :'))
+
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
